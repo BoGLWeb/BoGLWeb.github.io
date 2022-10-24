43fa6232281376fff8f092ecdefc88dc6326510f
Jakob Misbach <jakobmisbach8@gmail.com>
2022-10-24 14:10:00 -0400

    Fixed tests

---
+++ BoGLWeb/PlaywrightTests/RuleRuleSetLoadTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright.NUnit;
+using NuGet.Frameworks;
+using NUnit.Framework;
+
+namespace PlaywrightTests {
+    public class RuleRuleSetLoadTests{
+       
+        [SetUp]
+        public void setup() {
+        
+        }
+
+        [Test]
+        public void testNumLoadedRules() {
+            Assert.Pass();
+        }
+    }
+}
