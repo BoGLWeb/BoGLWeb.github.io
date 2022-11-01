ab44dec69b4346c7d5790c41026a2cbc8c8d467d
Jakob Misbach <jakobmisbach8@gmail.com>
2022-10-31 20:49:27 -0400

    Added classes for system diagram and bond graph

---
+++ BoGLWeb/BondGraph.cs
@@ -0,0 +1,54 @@
+﻿using GraphSynth.Representation;
+
+namespace BoGLWeb {
+    public class BondGraph {
+        public List<Element> elements;
+        public List<Bond> bonds;
+
+        public BondGraph() {
+            elements = new List<Element>();
+            bonds = new List<Bond>();
+        }
+
+        public void addElement(Element e) {
+            elements.Add(e);
+        }
+
+        public void addBond(Bond bond) {
+            bonds.Add(bond);
+        }
+
+        //Generate BondGraph from GraphSynth designGraph
+        //TODO Check if we can ensure a designGraph is a Bond Graph and produce error if it is not
+        public static BondGraph generateBondGraphFromGraphSynth(designGraph graph) {
+            return null;
+        }
+
+        public class Element {
+            private readonly string label;
+
+            public Element(string label) {
+                this.label = label;
+            }
+        }
+
+        public class Bond {
+            private Element source, sink;
+            private readonly string label;
+
+            //True means that the arrow is pointing towards the sink
+            private readonly bool direction;
+
+            //True means the causal stroke is at the source
+            private readonly bool causalSroke;
+
+            private Bond(Element source, Element sink, string label, bool direction, bool causalStroke) {
+                this.source = source;
+                this.sink = sink;
+                this.label = label;
+                this.direction = direction;
+                this.causalSroke = causalSroke;
+            }
+        }
+    }
+}
---
+++ BoGLWeb/SystemDiagram.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Playwright;
+
+namespace BoGLWeb {
+    public class SystemDiagram {
+        public List<Element> elements;
+        public List<Edge> edges;
+
+        public SystemDiagram() {
+            elements = new List<Element>();
+            edges = new List<Edge>();
+        }
+
+        public void addElement(string name) {
+            elements.Add(new Element(name));
+        }
+
+        public void addEdge(Element e1, Element e2) {
+            edges.Add(new Edge(e1, e2));
+        }
+
+
+        public class Element {
+            private string name;
+            private Dictionary<string, bool> modifiers;
+            private string velocityDir;
+
+            public Element(string name) {
+                this.name = name;
+                this.modifiers = generateModifierDictionary();
+                velocityDir = "";
+            }
+
+            //TODO Error checking
+            public void addModifier(string name) {
+                modifiers[name] = true;
+            }
+
+            //TODO Error checking
+            public void setVelocity(string vel) {
+                velocityDir = vel;
+            }
+        }
+
+        public static Dictionary<string, bool> generateModifierDictionary() {
+            Dictionary<string, bool> modifierDictionary = new Dictionary<string, bool>();
+            modifierDictionary.Add("Mass", false);
+            modifierDictionary.Add("Inertia", false);
+            modifierDictionary.Add("Stiffness", false);
+            modifierDictionary.Add("Friction", false);
+            modifierDictionary.Add("Damping", false);
+            modifierDictionary.Add("Parallel", false);
+            modifierDictionary.Add("Tooth Wear", false);
+            return modifierDictionary;
+        }
+
+        public class Edge {
+            private readonly Element e1;
+            private readonly Element e2;
+
+            public Edge(Element e1, Element e2) {
+                this.e1 = e1;
+                this.e2 = e2;
+            }
+        }
+    }
+}
