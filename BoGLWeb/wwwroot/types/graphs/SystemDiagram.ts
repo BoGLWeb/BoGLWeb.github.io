b03f300896ec59011774b4058ebebb904d8dbb14
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2022-11-16 14:35:50 -0500

    Added non-display graph types

---
+++ BoGLWeb/wwwroot/types/graphs/SystemDiagram.ts
@@ -0,0 +1,12 @@
+﻿import { GraphBond } from "../bonds/GraphBond";
+import { SystemDiagramElement } from "../elements/SystemDiagramElement";
+
+export class SystemDiagram {
+    nodes: SystemDiagramElement[];
+    edges: GraphBond[];
+
+    constructor(nodes: SystemDiagramElement[], edges: GraphBond[]) {
+        this.nodes = nodes;
+        this.edges = edges;
+    }
+}
