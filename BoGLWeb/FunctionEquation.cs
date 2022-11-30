0705633468cb68610f9b9676da269a4ddda7aed4
Anthony Vuolo <vuolo.969473@gmail.com>
2022-11-30 08:17:19 -0500

    Added universal ID to FunctionEquation

--- BoGLWeb/FunctionEquation.cs
+++ BoGLWeb/FunctionEquation.cs
@@ -1,7 +1,16 @@
 ﻿namespace BoGLWeb {
     namespace DifferentialEquationHelper {
         public class FunctionEquation {
-            private Function f1, f2;
+            /// <summary>
+            /// Stores the <c>Function</c> objects in this equation.
+            /// </summary>
+            private readonly Function f1, f2;
+
+            /// <summary>
+            /// Stores the static ID tracker and unique ID for this FunctionEquation.
+            /// </summary>
+            private static int universalID = 0;
+            private readonly int ID;
 
             /// <summary>
             /// Creates a new <c>FunctionEquation</c>.
@@ -15,6 +24,7 @@
             public FunctionEquation(Function f1, Function f2) {
                 this.f1 = f1;
                 this.f2 = f2;
+                this.ID = universalID++;
             }
 
             /// <summary>
@@ -33,6 +43,16 @@
                 } else {
                     return this.f2.Isolate(this.f1, var);
                 }
+            }
+
+            /// <summary>
+            /// Gets the hashCode for this <c>FunctionEquation</c>.
+            /// </summary>
+            /// <returns>
+            /// <c>this.ID</c>
+            /// </returns>
+            public override int GetHashCode() {
+                return this.ID;
             }
         }
     }
