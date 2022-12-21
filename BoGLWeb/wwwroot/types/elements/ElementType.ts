02b23366869797db2825388ab2560ac125e850a4
Jakob Misbach <jakobmisbach8@gmail.com>
2022-12-21 01:23:11 -0500

    Added max connections for elements

--- BoGLWeb/wwwroot/types/elements/ElementType.ts
+++ BoGLWeb/wwwroot/types/elements/ElementType.ts
@@ -6,7 +6,7 @@
     image: string;
     velocityAllowed: boolean;
 
-    constructor(id: number, name: string, category: number, image: string, allowedModifiers: number[], velocityAllowed: boolean) {
+    constructor(id: number, name: string, category: number, image: string, allowedModifiers: number[], velocityAllowed: boolean, maxConnections: number = Number.MAX_SAFE_INTEGER) {
         this.id = id;
         this.name = name;
         this.category = category;
