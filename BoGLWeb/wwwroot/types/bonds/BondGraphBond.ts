3b44c380d350d27d9aeae716ecb55ce3de0de114
Jakob Misbach <jakobmisbach8@gmail.com>
2023-02-03 11:27:10 -0500

    Updated TS to store dircetion

--- BoGLWeb/wwwroot/types/bonds/BondGraphBond.ts
+++ BoGLWeb/wwwroot/types/bonds/BondGraphBond.ts
@@ -4,10 +4,12 @@ import { GraphElement } from "../elements/GraphElement";
 export class BondGraphBond extends GraphBond {
     causalStroke: boolean;
     causalStrokeDirection: boolean;
+    hasDirection: boolean;
 
-    constructor(source: GraphElement, target: GraphElement, causalStroke: boolean, causalStrokeDirection: boolean, velocity: number = 0) {
+    constructor(source: GraphElement, target: GraphElement, causalStroke: boolean, causalStrokeDirection: boolean, hasDirection: boolean, velocity: number = 0) {
         super(source, target, velocity);
         this.causalStroke = causalStroke;
         this.causalStrokeDirection = causalStrokeDirection;
+        this.hasDirection = hasDirection;
     }
 }
