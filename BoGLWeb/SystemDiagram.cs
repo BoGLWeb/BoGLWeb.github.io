b18cbebaada566ccc50357d1289a20e9a88ec59b
Jakob Misbach <jakobmisbach8@gmail.com>
2023-02-23 08:55:54 -0500

    Updated error message wording

--- BoGLWeb/SystemDiagram.cs
+++ BoGLWeb/SystemDiagram.cs
@@ -245,8 +245,7 @@ namespace BoGLWeb {
         //Parsing
         //TODO Think about refactoring to use only one queue
         public static SystemDiagram generateSystemDiagramFromXML(string xml) {
-            string errorMessage =
-                "You have attempted to load an invalid .bogl file. Please ensure that you have the correct file and try again. File must have been saved using BoGL Web or BoGL Desktop to be valid.";
+            string errorMessage = "You have attempted to load an invalid .bogl file. Please ensure that you have the correct file and try again. File must have been saved using BoGL Web or BoGL Desktop to be valid.";
             List<string> tokens = tokenize(xml);
 
             //TODO Check if any of these are -1 because then we have an error
