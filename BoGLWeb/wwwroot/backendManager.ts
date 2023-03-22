b57aeb3e5d1089a8dffea8a21a1a5c82df9dd536
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2023-03-22 10:42:30 -0400

    Another change

--- BoGLWeb/wwwroot/backendManager.ts
+++ BoGLWeb/wwwroot/backendManager.ts
@@ -175,20 +175,6 @@ export namespace backendManager {
             };
         }
 
-       public fileToBase64 = (filename, filepath) => {
-            return new Promise(resolve => {
-                var file = new File([filename], filepath);
-                var reader = new FileReader();
-                // Read file content on file loaded event
-                reader.onload = function (event) {
-                    resolve(event.target.result);
-                };
-
-                // Convert data to base64 
-                reader.readAsDataURL(file);
-            });
-        };
-
         public applyInlineStyles(oldSVG: SVGSelection, svg: SVGSelection, graph: BaseGraphDisplay) {
             svg.selectAll(".link")
                 .style("fill", "none")
