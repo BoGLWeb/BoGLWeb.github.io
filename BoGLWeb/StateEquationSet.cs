eb52c3311e8519f62fa7af16017e373f67e7748e
Anthony Vuolo <vuolo.969473@gmail.com>
2023-03-28 16:51:39 -0400

    Renamed CausalPackager to CausalGraphWrapper and started final variable substitutions

--- BoGLWeb/StateEquationSet.cs
+++ BoGLWeb/StateEquationSet.cs
@@ -18,18 +18,18 @@ namespace BoGLWeb {
             /// <param name="graph">The target bond graph.</param>
             public StateEquationSet(BondGraph graph) {
                 int count = graph.GetDifferentialElements().Count;
-                BondGraph.BondGraphWrapper wrapper = new(graph);
-                List<FunctionEquation> initialEquations = GetInitialEquations(wrapper);
+                BondGraph.BondGraphWrapper graphWrapper = new(graph);
+                List<FunctionEquation> initialEquations = GetInitialEquations(graphWrapper);
                 this.initialEquations = new string[initialEquations.Count];
                 int initialIndex = 0;
                 foreach (FunctionEquation equation in initialEquations) {
                     this.initialEquations[initialIndex++] = equation.ToString();
                 }
-                List<CausalPackager> packagers = CausalPackager.GenerateList(wrapper);
+                List<CausalGraphWrapper> wrappers = CausalGraphWrapper.GenerateList(graphWrapper);
                 Dictionary<string, Expression> substitutionDictionary = new();
                 HashSet<string> usedSet = new();
-                foreach (CausalPackager packager in packagers) {
-                    KeyValuePair<string, Expression> pair = packager.GetExpression();
+                foreach (CausalGraphWrapper wrapper in wrappers) {
+                    KeyValuePair<string, Expression> pair = wrapper.GetExpression();
                     substitutionDictionary.Add(pair.Key, pair.Value);
                 }
                 GetRemainingGeneralizedSubstitutes(graph, substitutionDictionary);
@@ -61,7 +61,7 @@ namespace BoGLWeb {
             }
 
             /// <summary>
-            /// Gets the set of state finalDifferentialStateEquations from this <c>StateEquationSet</c>.
+            /// Gets the set of state equations from this <c>StateEquationSet</c>.
             /// </summary>
             /// <returns><c>this.finalDifferentialStateEquations</c></returns>
             public string[] GetFinalEquations() {
@@ -77,29 +77,29 @@ namespace BoGLWeb {
             }
 
             /// <summary>
-            /// Creates a causal packager that moves with or against the causal flow 
+            /// Creates a causal graphWrapper that moves with or against the causal flow 
             /// in the graph to create a map for differential equation generation.
-            /// Each <c>CausalPackager</c> is a transitive tree (directed connected 
+            /// Each <c>CausalGraphWrapper</c> is a transitive tree (directed connected 
             /// acyclic graph) that radiates outward from the element field.
             /// </summary>
-            private class CausalPackager {
+            private class CausalGraphWrapper {
                 // Stores the element at this node.
                 private readonly BondGraph.Element element;
                 // Stores the direction of energy flow.
                 private readonly bool isSource, isEffort;
                 // Stores all neighboring elements ahead in the flow.
-                private readonly List<CausalPackager> neighbors;
+                private readonly List<CausalGraphWrapper> neighbors;
                 // Stores the current state equation.
                 private Expression stateEquation;
 
                 /// <summary>
-                /// Creates a new CausalPackager with the specified criteria.
+                /// Creates a new CausalGraphWrapper with the specified criteria.
                 /// </summary>
                 /// <param name="element">The <c>BondGraph.Element</c> stored in this
-                /// <c>CausalPackager</c>.</param>
+                /// <c>CausalGraphWrapper</c>.</param>
                 /// <param name="isSource"><c>true</c> if this <c>Element</c> is
                 /// the source of the connector <c>Bond</c>, else <c>false</c>.</param>
-                private CausalPackager(BondGraph.Element element, bool isSource, bool isEffort) {
+                private CausalGraphWrapper(BondGraph.Element element, bool isSource, bool isEffort) {
                     this.element = element;
                     this.isSource = isSource;
                     this.isEffort = isEffort;
@@ -108,7 +108,7 @@ namespace BoGLWeb {
                 }
 
                 /// <summary>
-                /// Generates the variable name for the Expression associated with this CausalPackager.
+                /// Generates the variable name for the Expression associated with this CausalGraphWrapper.
                 /// </summary>
                 /// <returns></returns>
                 public string GenerateVariableName() {
@@ -116,19 +116,19 @@ namespace BoGLWeb {
                 }
 
                 /// <summary>
-                /// Recursively generates a new CausalPackager.
+                /// Recursively generates a new CausalGraphWrapper.
                 /// </summary>
                 /// <param name="element"></param>
                 /// <param name="isEffort"></param>
                 /// <param name="bondsBySource"></param>
                 /// <param name="bondsByTarget"></param>
-                private static CausalPackager GeneratePackager(BondGraph.Element element, bool isEffort, Dictionary<int, List<BondGraph.Bond>> bondsBySource, Dictionary<int, List<BondGraph.Bond>> bondsByTarget) {
-                    CausalPackager returnValue = new(element, true, isEffort);
-                    Stack<CausalPackager> packagerStack = new(new[] { returnValue });
+                private static CausalGraphWrapper GenerateWrapper(BondGraph.Element element, bool isEffort, Dictionary<int, List<BondGraph.Bond>> bondsBySource, Dictionary<int, List<BondGraph.Bond>> bondsByTarget) {
+                    CausalGraphWrapper returnValue = new(element, true, isEffort);
+                    Stack<CausalGraphWrapper> wrapperStack = new(new[] { returnValue });
                     HashSet<int> used = new();
-                    while (packagerStack.Count > 0) {
-                        CausalPackager packager = packagerStack.Pop();
-                        int elementID = packager.element.GetID();
+                    while (wrapperStack.Count > 0) {
+                        CausalGraphWrapper wrapper = wrapperStack.Pop();
+                        int elementID = wrapper.element.GetID();
                         if (used.Contains(elementID)) {
                             throw new Exception("State finalDifferentialStateEquations cannot be derived in looped causality.");
                         }
@@ -137,9 +137,9 @@ namespace BoGLWeb {
                         if (bondsToTarget != null) {
                             foreach (BondGraph.Bond bond in bondsToTarget) {
                                 if (isEffort == bond.GetCausalDirection()) {
-                                    CausalPackager neighborPackager = new(bond.getSink(), true, isEffort);
-                                    packager.neighbors.Add(neighborPackager);
-                                    packagerStack.Push(neighborPackager);
+                                    CausalGraphWrapper neighborWrapper = new(bond.getSink(), true, isEffort);
+                                    wrapper.neighbors.Add(neighborWrapper);
+                                    wrapperStack.Push(neighborWrapper);
                                 }
                             }
                         }
@@ -147,9 +147,9 @@ namespace BoGLWeb {
                         if (bondsToSource != null) {
                             foreach (BondGraph.Bond bond in bondsToSource) {
                                 if (isEffort ^ bond.GetCausalDirection()) {
-                                    CausalPackager neighborPackager = new(bond.getSource(), false, isEffort);
-                                    packager.neighbors.Add(neighborPackager);
-                                    packagerStack.Push(neighborPackager);
+                                    CausalGraphWrapper neighborWrapper = new(bond.getSource(), false, isEffort);
+                                    wrapper.neighbors.Add(neighborWrapper);
+                                    wrapperStack.Push(neighborWrapper);
                                 }
                             }
                         }
@@ -158,19 +158,19 @@ namespace BoGLWeb {
                 }
 
                 /// <summary>
-                /// Creates a new <c>CausalPackager</c> from a <c>BondGraph</c>.
+                /// Creates a new <c>CausalGraphWrapper</c> from a <c>BondGraph</c>.
                 /// </summary>
-                /// <param name="wrapper">The model bond graph wrapper.</param>
-                public static List<CausalPackager> GenerateList(BondGraph.BondGraphWrapper wrapper) {
-                    List<CausalPackager> packagerList = new();
+                /// <param name="wrapper">The model bond graph graphWrapper.</param>
+                public static List<CausalGraphWrapper> GenerateList(BondGraph.BondGraphWrapper wrapper) {
+                    List<CausalGraphWrapper> wrapperList = new();
                     foreach (KeyValuePair<string, BondGraph.Element> pair in wrapper.GetElements()) {
                         char indicator = pair.Value.GetTypeChar();
                         if ("IRC".Contains(indicator)) {
-                            packagerList.Add(GeneratePackager(pair.Value, indicator == 'I', 
+                            wrapperList.Add(GenerateWrapper(pair.Value, indicator == 'I', 
                                 wrapper.GetBondsBySource(), wrapper.GetBondsByTarget()));
                         }
                     }
-                    return packagerList;
+                    return wrapperList;
                 }
 
                 /// <summary>
@@ -178,51 +178,51 @@ namespace BoGLWeb {
                 /// </summary>
                 /// <returns></returns>
                 public KeyValuePair<string, Expression> GetExpression() {
-                    Stack<CausalPackager> packageStack = new(new[] { this });
+                    Stack<CausalGraphWrapper> packageStack = new(new[] { this });
                     Stack<bool> checkStack = new(new[] { false });
                     while (packageStack.Count > 0) {
-                        CausalPackager packager = packageStack.Pop();
+                        CausalGraphWrapper wrapper = packageStack.Pop();
                         if (checkStack.Pop()) {
-                            if (packager.neighbors.Count > 0) {
-                                packager.stateEquation = new Expression();
-                                CausalPackager loneChild = packager.neighbors[0];
-                                char typeChar = packager.element.GetTypeChar();
+                            if (wrapper.neighbors.Count > 0) {
+                                wrapper.stateEquation = new Expression();
+                                CausalGraphWrapper loneChild = wrapper.neighbors[0];
+                                char typeChar = wrapper.element.GetTypeChar();
                                 switch (typeChar) {
                                     case '0':
                                     case '1':
-                                        if (typeChar == '1' == packager.isEffort) {
-                                            foreach (CausalPackager child in packager.neighbors) {
-                                                if (packager.isSource == child.isSource) {
-                                                    packager.stateEquation = packager.stateEquation.Add(child.stateEquation);
+                                        if (typeChar == '1' == wrapper.isEffort) {
+                                            foreach (CausalGraphWrapper child in wrapper.neighbors) {
+                                                if (wrapper.isSource == child.isSource) {
+                                                    wrapper.stateEquation = wrapper.stateEquation.Add(child.stateEquation);
                                                 } else {
-                                                    packager.stateEquation = packager.stateEquation.Subtract(child.stateEquation);
+                                                    wrapper.stateEquation = wrapper.stateEquation.Subtract(child.stateEquation);
                                                 }
                                                 child.stateEquation = new(child.GenerateVariableName());
                                             }
                                         } else {
-                                            packager.stateEquation = loneChild.stateEquation;
+                                            wrapper.stateEquation = loneChild.stateEquation;
                                             loneChild.stateEquation = new(loneChild.GenerateVariableName());
                                         }
                                         break;
                                     case 'T':
-                                        Expression expr = new("T" + packager.element.GetID());
+                                        Expression expr = new("T" + wrapper.element.GetID());
                                         if (isEffort == isSource) {
-                                            packager.stateEquation = loneChild.stateEquation.Multiply(expr);
+                                            wrapper.stateEquation = loneChild.stateEquation.Multiply(expr);
                                         } else {
-                                            packager.stateEquation = loneChild.stateEquation.Divide(expr);
+                                            wrapper.stateEquation = loneChild.stateEquation.Divide(expr);
                                         }
                                         loneChild.stateEquation = new(loneChild.GenerateVariableName());
                                         break;
                                     default:
-                                        packager.stateEquation = loneChild.stateEquation;
+                                        wrapper.stateEquation = loneChild.stateEquation;
                                         loneChild.stateEquation = new(loneChild.GenerateVariableName());
                                         break;
                                 }
                             }
                         } else {
-                            packageStack.Push(packager);
+                            packageStack.Push(wrapper);
                             checkStack.Push(true);
-                            foreach (CausalPackager child in packager.neighbors) {
+                            foreach (CausalGraphWrapper child in wrapper.neighbors) {
                                 packageStack.Push(child);
                                 checkStack.Push(false);
                             }
@@ -235,22 +235,22 @@ namespace BoGLWeb {
                 }
 
                 /// <summary>
-                /// Converts this <c>CausalPackager</c> to a printable format.
+                /// Converts this <c>CausalGraphWrapper</c> to a printable format.
                 /// </summary>
-                /// <returns>This <c>CausalPackager</c> as a <c>string</c></returns>
+                /// <returns>This <c>CausalGraphWrapper</c> as a <c>string</c></returns>
                 public override string ToString() {
                     StringBuilder print = new();
-                    Stack<CausalPackager> packageStack = new(new[] {this});
+                    Stack<CausalGraphWrapper> packageStack = new(new[] {this});
                     Stack<bool> checkStack = new(new[] {false});
                     while (packageStack.Count > 0) {
-                        CausalPackager packager = packageStack.Pop();
+                        CausalGraphWrapper wrapper = packageStack.Pop();
                         if (checkStack.Pop()) {
                             print.Append(']');
                         } else {
-                            print.Append("[ ").Append(packager.element).Append(' ');
-                            packageStack.Push(packager);
+                            print.Append("[ ").Append(wrapper.element).Append(' ');
+                            packageStack.Push(wrapper);
                             checkStack.Push(true);
-                            foreach (CausalPackager neighbor in packager.neighbors) {
+                            foreach (CausalGraphWrapper neighbor in wrapper.neighbors) {
                                 packageStack.Push(neighbor);
                                 checkStack.Push(false);
                             }
@@ -263,7 +263,7 @@ namespace BoGLWeb {
             /// <summary>
             /// Loads the initial equations for this <c>StateEquationSet</c>.
             /// </summary>
-            /// <param name="wrapper">The wrapper containing the information
+            /// <param name="wrapper">The graphWrapper containing the information
             /// for the target bond graph.</param>
             private static List<FunctionEquation> GetInitialEquations(BondGraph.BondGraphWrapper wrapper) {
                 Dictionary<int, List<BondGraph.Bond>> bbs = wrapper.GetBondsBySource();
@@ -275,10 +275,10 @@ namespace BoGLWeb {
                         case '0':
                         case '1':
                             bool isOneJunc = typeChar == '1';
-                            String zeroSum = "F", equal = "E";
+                            String zeroSum = "f", equal = "e";
                             if (isOneJunc) {
-                                zeroSum = "E";
-                                equal = "F";
+                                zeroSum = "e";
+                                equal = "f";
                             }
                             Expression negSum = new("0"), posSum = new("0");
                             int inwardCausalBondCount = 0, outwardCausalBondCount = 0;
@@ -346,6 +346,18 @@ namespace BoGLWeb {
                             break;
                     }
                 }
+            }
+
+            /// <summary>
+            /// Gets a mapping for domain variables.
+            /// </summary>
+            /// <param name="wrapper">The <c>BondGraphWrapper</c> containing information on 
+            /// connections between the </param>
+            /// <returns>The <c>Dictionary</c> mapping.</returns>
+            public static Dictionary<string, string> GetDomainVariables(BondGraph.BondGraphWrapper wrapper) {
+                Dictionary<string, string> vars = new();
+                //
+                return vars;
             }
         }
     }
