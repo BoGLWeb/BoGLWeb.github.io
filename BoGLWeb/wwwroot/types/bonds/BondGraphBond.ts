11e340b019d4a0b4dda1eb5e2ea77d108735323b
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2023-04-17 01:03:36 -0400

    Fed effort and flow labels in through backend

--- BoGLWeb/wwwroot/types/bonds/BondGraphBond.ts
+++ BoGLWeb/wwwroot/types/bonds/BondGraphBond.ts
@@ -9,13 +9,13 @@ export class BondGraphBond extends GraphBond {
     flowLabel: string;
     id: number = 0;
 
-    constructor(id: number, source: GraphElement, target: GraphElement, causalStroke: boolean, causalStrokeDirection: boolean, hasDirection: boolean, velocity: number = 0) {
+    constructor(id: number, source: GraphElement, target: GraphElement, causalStroke: boolean, causalStrokeDirection: boolean, hasDirection: boolean, effortLabel: string, flowLabel: string, velocity: number = 0) {
         super(source, target, velocity);
         this.id = id;
         this.causalStroke = causalStroke;
         this.causalStrokeDirection = causalStrokeDirection;
         this.hasDirection = hasDirection;
-        this.effortLabel = "e";
-        this.flowLabel = "f";
+        this.effortLabel = effortLabel;
+        this.flowLabel = flowLabel;
     }
 }
