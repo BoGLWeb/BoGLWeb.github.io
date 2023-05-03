49b82d12fe9b6592091e6b344848b7ca79703c64
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2023-05-03 16:16:26 -0400

    Commented main.ts

--- BoGLWeb/wwwroot/main.ts
+++ BoGLWeb/wwwroot/main.ts
@@ -1,17 +1,156 @@
 "use strict";
-import { ElementNamespace } from "./types/elements/ElementNamespace";
-import { SystemDiagramDisplay } from "./types/display/SystemDiagramDisplay";
 import { backendManager } from "./backendManager";
+import { SubmenuID } from "./types/display/BondGraphBond";
+import { SystemDiagramDisplay } from "./types/display/SystemDiagramDisplay";
+import { ElementNamespace } from "./types/elements/ElementNamespace";
 import { SystemDiagram } from "./types/graphs/SystemDiagram";
 import getBackendManager = backendManager.getBackendManager;
-import { SubmenuID } from "./types/display/BondGraphBond";
 
-export function populateMenu() {
+var topMenuButtons;
+let hasAssignedInputClick = false;
+var menuClickingDone = false;
+
+// lists the DOM ids of menus by their id number
+var menuIdMap = {
+    0: "fileMenu",
+    1: "editMenu",
+    2: "helpMenu",
+    3: "exampleMenu",
+    4: "mechTransMenu",
+    5: "mechRotMenu",
+    6: "elecMenu"
+}
+
+// lists the index numbers and DOM id numbers of submenu children by their parent's id number
+var submenuMap = {
+    2: [new SubmenuID(3, 3)],
+    3: [new SubmenuID(1, 4), new SubmenuID(2, 5), new SubmenuID(3, 6)]
+}
+
+// loads a system diagram from a string
+async function loadSystemDiagram(text: string) {
+    let systemDiagramText = await DotNet.invokeMethodAsync("BoGLWeb", "openSystemDiagram", text);
+    if (systemDiagramText != null) {
+        getBackendManager().loadSystemDiagram(systemDiagramText);
+    }
+}
+
+// waits until a menu is clicked
+function waitForMenuClickingDone(func) {
+    if (menuClickingDone) {
+        func();
+    } else {
+        setTimeout(() => waitForMenuClickingDone(func), 20);
+    }
+}
+
+// finds the first parent menu of a submenu
+function findParentMenu(menuId: number) {
+    for (let key of Object.keys(submenuMap)) {
+        if ((submenuMap[key] as SubmenuID[]).some(sub => sub.id == menuId)) {
+            return parseInt(key);
+        }
+    }
+    return null;
+}
+
+// finds all parent menus of a submenu recursively
+function findAllParentMenus(menuId: number) {
+    let parent = findParentMenu(menuId);
+    if (parent != null) {
+        return [parent, ...findAllParentMenus(parent)];
+    }
+    return [];
+}
+
+// add click action to each top menu and submenu
+function menuClickAction(menuTitle: Node, k: number) {
+    menuTitle.addEventListener("click", (e) => {
+        e.stopPropagation();
+        let parents = findAllParentMenus(k);
+
+        waitForMenuClickingDone(() => {
+            // assign onchange event for file upload input in File menu once it exists
+            if (k == 0 && !hasAssignedInputClick) {
+                hasAssignedInputClick = true;
+                let input = document.getElementById("fileUpload") as HTMLInputElement;
+                input.onchange = async () => {
+                    let files = Array.from(input.files);
+                    if (files[0].text) {
+                        let text = await files[0].text();
+                        loadSystemDiagram(text);
+                    } else {
+                        const reader = new FileReader();
+                        reader.onload = event => {
+                            loadSystemDiagram(event.target.result as string);
+                        };
+                        reader.readAsText(files[0]);
+                    }
+                }
+            }
+
+            let el = document.getElementById(menuIdMap[k]);
+            if (el) {
+                el = el.parentElement?.parentElement;
+                // toggle whether clicked menu is hidden
+                el.setAttribute("hidden-menu", (el.getAttribute("hidden-menu") == "false").toString());
+                // if menu is a submenu, place it appropriately by its parent
+                if (![0, 1, 2].includes(k)) {
+                    let menuTitleBounds = (menuTitle as Element).getBoundingClientRect();
+                    el.style.top = menuTitleBounds.top + "px";
+                    el.style.left = (menuTitleBounds.left + menuTitleBounds.width + 4) + "px";
+                }
+
+                // if menu is a submenu and is visible, add click actions to submenu buttons now that they are visible
+                if (el.getAttribute("hidden-menu") == "false" && submenuMap.hasOwnProperty(k)) {
+                    for (let sub of submenuMap[k] as SubmenuID[]) {
+                        if (!sub.hasClickAction) {
+                            let el = document.getElementById(menuIdMap[k]).parentElement.children[sub.index];
+                            menuClickAction(el, sub.id);
+                            sub.hasClickAction = true;
+                        }
+                    }
+                }
+
+                // hide all menus that are not the current menu or one of its parents
+                for (let i = 0; i < Object.keys(menuIdMap).length; i++) {
+                    el = document.getElementById(menuIdMap[i]);
+                    if (i == k || parents.includes(i) || !el) continue;
+                    el = el.parentElement.parentElement;
+                    el.setAttribute("hidden-menu", "true");
+                }
+            }
+        });
+    });
+}
+
+// clicks through all menus to get them in the DOM
+function clickSubmenus(menuId: number) {
+    const cond = document.getElementById(menuIdMap[menuId])?.parentElement?.parentElement;
+
+    if (cond) {
+        for (let submenu of submenuMap[menuId] as SubmenuID[]) {
+            let submenuEl = document.getElementById(menuIdMap[menuId]).parentElement.children[submenu.index];
+            (submenuEl as HTMLElement).click();
+            clickSubmenus(submenu.id);
+        }
+    } else if (submenuMap.hasOwnProperty(menuId)) {
+        setTimeout(() => clickSubmenus(menuId), 20);
+    }
+    if (menuId == 6) {
+        menuClickingDone = true;
+    }
+}
+
+// populates the element menu with buttons representing each element
+function populateElementMenu() {
+    // iterates through element categories and types to dynamically generate the element menu, making it easy to add new elements
     ElementNamespace.categories.map((c, i) => {
         ElementNamespace.elementTypes.filter(e => e.category === i).forEach(e => {
             const group = document.createElement('div');
 
             group.classList.add("groupDiv");
+            // if the element is dragged, store it in system diagram
             group.addEventListener("mousedown", function () {
                 document.body.style.cursor = "grabbing";
                 window.systemDiagram.draggingElement = e.id;
@@ -19,6 +158,7 @@ export function populateMenu() {
 
             document.getElementById(c.folderName).appendChild(group);
 
+            // make HTML object for element with box and element image
             var box = document.createElement('div');
             box.classList.add("box");
             group.appendChild(box);
@@ -33,13 +173,12 @@ export function populateMenu() {
     });
 }
 
-var topMenuButtons;
-
+// completes all actions that need to happen before the user interacts with the page
 async function loadPage() {
     delete window.jQuery;
-    window.tabNum = "1"; 
+    window.tabNum = "1";
     let sliderHolder = document.querySelector("#zoomMenu .ant-slider-handle");
-    let sliderImg: any = document.createElement("img"); 
+    let sliderImg: any = document.createElement("img");
     sliderImg.src = "images/sliderIcon.svg";
     sliderImg.id = "sliderImg";
     sliderImg.draggable = false;
@@ -49,24 +188,29 @@ async function loadPage() {
     window.systemDiagramSVG = d3.select("#systemDiagram").append("svg");
     window.systemDiagramSVG.classed("graphSVG", true);
 
+    // looks as URL and checks whether a system diagram needs to be loaded from URL
     const urlParams = new URLSearchParams(window.location.search);
     const myParam = urlParams.get('q');
-    if (myParam !== null){
-        let sysDiagramString  = await DotNet.invokeMethodAsync("BoGLWeb", "uncompressUrl", myParam);
+    if (myParam !== null) {
+        // if URL has system diagram, load that system diagram
+        let sysDiagramString = await DotNet.invokeMethodAsync("BoGLWeb", "uncompressUrl", myParam);
         getBackendManager().loadSystemDiagram(sysDiagramString);
     } else {
+        // if URL has no system diagram, load an empty systemm diagram
         window.systemDiagram = new SystemDiagramDisplay(window.systemDiagramSVG, new SystemDiagram([], []));
         window.systemDiagram.updateGraph();
         backendManager.getBackendManager().zoomCenterGraph("1");
         window.systemDiagram.changeScale(window.systemDiagram.svgX, window.systemDiagram.svgY, 1);
     }
 
+    // on mouseup, clear element dragged from menu
     document.addEventListener("mouseup", function () {
         document.body.style.cursor = "auto";
         window.systemDiagram.draggingElement = null;
     });
 
-    populateMenu();
+    // populate the element menu
+    populateElementMenu();
 
     window.unsimpBGSVG = d3.select("#unsimpBG").append("svg");
     window.unsimpBGSVG.classed("graphSVG", true);
@@ -75,6 +219,7 @@ async function loadPage() {
     window.causalBGSVG = d3.select("#causalBG").append("svg");
     window.causalBGSVG.classed("graphSVG", true);
 
+    // call SVG keyup and keydown functions on keyup or keydown
     d3.select(window).on("keydown", function () {
         let graph = backendManager.getBackendManager().getGraphByIndex(window.tabNum);
         graph.svgKeyDown.call(graph);
@@ -86,12 +231,14 @@ async function loadPage() {
 
     topMenuButtons = document.getElementsByClassName('topMenu');
 
+    // open all top menus and click their submenus recursively to load them in the DOM, then add click actions to these menus
     for (let i = 0; i < 3; i++) {
         topMenuButtons.item(i).click();
         clickSubmenus(i);
         menuClickAction(topMenuButtons.item(i), i);
     }
 
+    // when anywhere outside a top menu is clicked, close all top menus and submenus
     document.getElementsByClassName("page").item(0).addEventListener("click", () => {
         for (let i = 0; i < Object.keys(menuIdMap).length; i++) {
             let el = document.getElementById(menuIdMap[i]);
@@ -101,10 +248,12 @@ async function loadPage() {
         }
     });
 
+    // add an alert before leaving the page prompting the user to save unfinished work
     window.onbeforeunload = function (e) {
         return "Are you sure you want to exit BoGL Web? Your current progress will be lost unless you download it or make a URL from it.";
     };
 
+    // handle left menu resize for all tabs
     const ele = document.getElementById('graphMenu');
     let x = 0;
     let w = 0;
@@ -135,116 +284,23 @@ async function loadPage() {
         resizer.addEventListener('mousedown', mouseDownHandler);
     });
 
+    // download all example .bogl files to allow them to be cached for use in PWA mode
     let examples = ["basic-two-mass-system", "basic-two-mass-system1", "basic-two-mass-system2", "masses_on_a_spring", "moving_masses", "spring_&_damper", "rack_pinion", "motor-gear-pair", "lrc_circuit"];
 
     for (let i in examples) {
         fetch("https://boglweb.github.io/rules-and-examples/examples/" + examples[i] + ".bogl");
     }
 
+    // add mouseenter event to all checkboxes that focuses the checkbox before clicking on it, fixes a bug
     document.querySelectorAll('.ant-checkbox-wrapper').forEach(e => e.addEventListener("mouseenter", () => (e.children[0].children[0] as HTMLElement).focus()));
 
+    // closes the zoom menu by default if the screen height is too small
     if (innerHeight < 635) {
         (document.querySelector("#zoomMenu > div > div > div") as HTMLElement).click();
     }
 }
 
-var menuIdMap = {
-    0: "fileMenu",
-    1: "editMenu",
-    2: "helpMenu",
-    3: "exampleMenu",
-    4: "mechTransMenu",
-    5: "mechRotMenu",
-    6: "elecMenu"
-}
-
-// holds the index numbers of submenu children
-var submenuMap = {
-    2: [new SubmenuID(3, 3)],
-    3: [new SubmenuID(1, 4), new SubmenuID(2, 5), new SubmenuID(3, 6)]
-}
-
-var menuClickingDone = false;
-
-function findParentMenu(menuId: number) {
-    for (let key of Object.keys(submenuMap)) {
-        if ((submenuMap[key] as SubmenuID[]).some(sub => sub.id == menuId)) {
-            return parseInt(key);
-        }
-    }
-    return null;
-}
-
-function findAllParentMenus(menuId: number) {
-    let parent = findParentMenu(menuId);
-    if (parent != null) {
-        return [parent, ...findAllParentMenus(parent)];
-    }
-    return [];
-}
-
-let hasAssignedInputClick = false;
-
-async function loadSystemDiagram(text: string) {
-    let systemDiagramText = await DotNet.invokeMethodAsync("BoGLWeb", "openSystemDiagram", text);
-    if (systemDiagramText != null) {
-        getBackendManager().loadSystemDiagram(systemDiagramText);
-    }
-}
-
-function menuClickAction(menuTitle: Node, k: number) {
-    menuTitle.addEventListener("click", (e) => {
-        e.stopPropagation();
-        let parents = findAllParentMenus(k);
-        waitForMenuClickingDone(() => {
-            if (k == 0 && !hasAssignedInputClick) {
-                hasAssignedInputClick = true;
-                let input = document.getElementById("fileUpload") as HTMLInputElement;
-                input.onchange = async () => {
-                    let files = Array.from(input.files);
-                    if (files[0].text) {
-                        let text = await files[0].text();
-                        loadSystemDiagram(text);
-                    } else {
-                        const reader = new FileReader();
-                        reader.onload = event => {
-                            loadSystemDiagram(event.target.result as string);
-                        };
-                        reader.readAsText(files[0]);
-                    }
-                }
-            }
-            let el = document.getElementById(menuIdMap[k]);
-            if (el) {
-                el = el.parentElement?.parentElement;
-                el.setAttribute("hidden-menu", (el.getAttribute("hidden-menu") == "false").toString());
-                if (![0, 1, 2].includes(k)) {
-                    let menuTitleBounds = (menuTitle as Element).getBoundingClientRect();
-                    el.style.top = menuTitleBounds.top + "px";
-                    el.style.left = (menuTitleBounds.left + menuTitleBounds.width + 4) + "px";
-                }
-
-                if (el.getAttribute("hidden-menu") == "false" && submenuMap.hasOwnProperty(k)) {
-                    for (let sub of submenuMap[k] as SubmenuID[]) {
-                        if (!sub.hasClickAction) {
-                            let el = document.getElementById(menuIdMap[k]).parentElement.children[sub.index];
-                            menuClickAction(el, sub.id);
-                            sub.hasClickAction = true;
-                        }
-                    }
-                }
-
-                for (let i = 0; i < Object.keys(menuIdMap).length; i++) {
-                    el = document.getElementById(menuIdMap[i]);
-                    if (i == k || parents.includes(i) || !el) continue;
-                    el = el.parentElement.parentElement;
-                    el.setAttribute("hidden-menu", "true");
-                }
-            }
-        });
-    });
-}
-
+// poll the DOM until the graph menu and top menu have loaded, then call the load page function
 function pollDOM() {
     const el = document.getElementById('graphMenu') && document.getElementsByClassName('topMenu').length > 0;
 
@@ -255,31 +311,5 @@ function pollDOM() {
     }
 }
 
-// clicks through all menus to get them in the DOM
-function clickSubmenus(menuId: number) {
-    const cond = document.getElementById(menuIdMap[menuId])?.parentElement?.parentElement;
-
-    if (cond) {
-        for (let submenu of submenuMap[menuId] as SubmenuID[]) {
-            let submenuEl = document.getElementById(menuIdMap[menuId]).parentElement.children[submenu.index];
-            (submenuEl as HTMLElement).click();
-            clickSubmenus(submenu.id);
-        }
-    } else if (submenuMap.hasOwnProperty(menuId)) {
-        setTimeout(() => clickSubmenus(menuId), 20);
-    }
-    if (menuId == 6) {
-        menuClickingDone = true;
-    }
-}
-
-function waitForMenuClickingDone(func) {
-    if (menuClickingDone) {
-        func();
-    } else {
-        setTimeout(() => waitForMenuClickingDone(func), 20);
-    }
-}
-
-
+// starts the DOM polling
 pollDOM();
