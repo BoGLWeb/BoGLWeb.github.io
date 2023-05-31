a52cacc6e1e21402745a1246533987d1e7397032
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2023-05-31 12:38:35 -0400

    Finished commenting graph bond classes

--- BoGLWeb/wwwroot/types/bonds/GraphBond.ts
+++ BoGLWeb/wwwroot/types/bonds/GraphBond.ts
@@ -1,9 +1,10 @@
 ﻿import { GraphElement } from "../elements/GraphElement";
 
+// provides the UI with information about a connection in a system diagram or bond in a bond graph
 export class GraphBond {
     source: GraphElement;
     target: GraphElement;
-    velocity: number;
+    velocity: number; // set to 0 for bond graph bonds
 
     constructor(source: GraphElement, target: GraphElement, velocity = 0) {
         this.source = source;
