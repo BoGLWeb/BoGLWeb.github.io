4731f0fc632ca08844887be5d3f436daf1c40476
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2023-05-31 13:23:07 -0400

    Finished commenting elements

--- BoGLWeb/wwwroot/types/elements/BondGraphElement.ts
+++ BoGLWeb/wwwroot/types/elements/BondGraphElement.ts
@@ -1,9 +1,10 @@
 ﻿import { GraphElement } from "./GraphElement";
 
+// provides info about a bond graph element
 export class BondGraphElement extends GraphElement {
-    label: string;
-    labelSize: { width: number, height: number } = null;
-    backendId: number;
+    label: string; // label for bond graph element
+    labelSize: { width: number, height: number } = null; // size of bond graph label
+    backendId: number; // ID for this element in the backend
 
     constructor(id: number, backendId: number, label: string, x: number, y: number) {
         super(id, x, y);
--- BoGLWeb/wwwroot/types/elements/ElementType.ts
+++ BoGLWeb/wwwroot/types/elements/ElementType.ts
@@ -1,12 +1,13 @@
-﻿export class ElementType {
+﻿// provide info about an element type
+export class ElementType {
     id: number;
-    name: string;
-    category: number;
-    allowedModifiers: number[];
-    image: string;
-    velocityAllowed: boolean;
-    maxConnections: number;
-    isMultiElement: boolean;
+    name: string; // name of element type
+    category: number; // category of element type
+    allowedModifiers: number[]; // allowed modifiers for an element type
+    image: string; // image for the element type used in the element menu and in the system diagram SVG
+    velocityAllowed: boolean; // whether the element can have a velocity
+    maxConnections: number; // maximum connections that an element can have
+    isMultiElement: boolean; // whether the element is multi-element, a combination of elements with a name an image
 
     constructor(id: number, name: string, category: number, image: string, allowedModifiers: number[], velocityAllowed: boolean, maxConnections: number = Number.MAX_SAFE_INTEGER, isMultiElement: boolean = false) {
         this.id = id;
